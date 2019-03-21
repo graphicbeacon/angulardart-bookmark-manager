08f699bb4da51458db5be06a3cacf327ff9856a4
Jermaine Oppong <creativebracket@gmail.com>
2019-03-21 14:20:29 +0000

    Implements edit and add functionality for bookmarks.

--- lib/src/bm_scaffold/bm_scaffold_component.dart
+++ lib/src/bm_scaffold/bm_scaffold_component.dart
@@ -1,28 +1,64 @@
 import 'package:angular/angular.dart';
+import 'package:angular_forms/angular_forms.dart';
+
+import '../model/bookmark.dart';
 
 @Component(
-    selector: 'bm-scaffold',
-    templateUrl: 'bm_scaffold_component.html',
-    directives: [coreDirectives])
+  selector: 'bm-scaffold',
+  templateUrl: 'bm_scaffold_component.html',
+  directives: [coreDirectives, formDirectives],
+)
 class BookmarkScaffoldComponent {
   final List bookmarks = [
-    {
-      'title': 'Creative Bracket',
-      'description':
+    Bookmark(
+      title: 'Creative Bracket',
+      description:
           'Go-to Dart blog containing Dart resources for beginners and beyond',
-      'url': 'https://creativebracket.com',
-    },
-    {
-      'title': 'Dartlang Home',
-      'description': 'Documentation website for the Dart language and tools',
-      'url': 'https://dartlang.org',
-    },
-    {
-      'title': 'Flutter',
-      'description': 'Build native mobile apps with the Flutter SDK',
-      'url': 'https://flutter.io',
-    }
+      url: 'https://creativebracket.com',
+      edit: false,
+    ),
+    Bookmark(
+      title: 'Dartlang Home',
+      description: 'Documentation website for the Dart language and tools',
+      url: 'https://dartlang.org',
+      edit: false,
+    ),
+    Bookmark(
+      title: 'Flutter',
+      description: 'Build native mobile apps with the Flutter SDK',
+      url: 'https://flutter.io',
+      edit: false,
+    )
   ];
+
+  Bookmark editedBookmark;
+
+  addBookmark() {
+    var bm = Bookmark();
+    bookmarks.add(bm);
+    editedBookmark = Bookmark(
+      title: bm.title,
+      description: bm.description,
+      url: bm.url,
+    );
+  }
+
+  editBookmark(int index) {
+    editedBookmark = Bookmark(
+      title: bookmarks[index].title,
+      description: bookmarks[index].description,
+      url: bookmarks[index].url,
+      edit: bookmarks[index].edit,
+    );
+    bookmarks[index].edit = true;
+  }
+
+  updateBookmark(int index) {
+    bookmarks[index].title = editedBookmark.title;
+    bookmarks[index].description = editedBookmark.description;
+    bookmarks[index].url = editedBookmark.url;
+    bookmarks[index].edit = false;
+  }
 
   removeBookmark(int index) {
     bookmarks.removeAt(index);
---
+++ lib/src/model/bookmark.dart
@@ -0,0 +1,13 @@
+class Bookmark {
+  Bookmark({
+    this.title = '',
+    this.description = '',
+    this.url = '',
+    this.edit = true,
+  });
+
+  String title;
+  String description;
+  String url;
+  bool edit;
+}
